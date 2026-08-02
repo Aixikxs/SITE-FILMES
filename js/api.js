a85c34291ef9184fc3321ad859f18dc5c5600d60
Aixikxs <133054601+Aixikxs@users.noreply.github.com>
2026-08-02 15:05:08 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,11 +1,41 @@
 const API_URL = "https://superflixapi.pro";
 
+async function api(path) {
+    const response = await fetch(`${API_URL}${path}`);
+
+    if (!response.ok) {
+        throw new Error("Erro na API");
+    }
+
+    return await response.json();
+}
+
+// Lista de filmes
 async function getMovies() {
-    const response = await fetch(
-        `${API_URL}/lista?category=filme&type=tmdb&format=json`
-    );
+    return await api("/lista?category=filme&type=tmdb&format=json");
+}
 
-    const ids = await response.json();
+// Informações de um filme
+async function getMovie(id) {
+    return await api(`/filme/${id}`);
+}
 
-    return ids;
+// Lista de séries
+async function getSeries() {
+    return await api("/lista?category=serie&type=tmdb&format=json");
+}
+
+// Informações da série
+async function getSerie(id) {
+    return await api(`/serie/${id}`);
+}
+
+// Lista de animes
+async function getAnimes() {
+    return await api("/lista?category=anime&type=tmdb&format=json");
+}
+
+// Informações do anime
+async function getAnime(id) {
+    return await api(`/anime/${id}`);
 }
